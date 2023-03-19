1f6867ecd7b756faa0847007d82f677eb8582c42
Kirill <bloodwingest@gmail.com>
2023-03-19 23:57:10 +0300

    feat: add card component and songs base

---
+++ first-react-app/src/songsList.ts
@@ -0,0 +1,18 @@
+export const songs = [
+  {
+    id: 1,
+    name: "testName1",
+    artist: "testArtist1",
+    image: "testImage1",
+    genre: "testGenre1",
+    album: "testAlbum1",
+  },
+  {
+    id: 2,
+    name: "testName2",
+    artist: "testArtist2",
+    image: "testImage2",
+    genre: "testGenre2",
+    album: "testAlbum2",
+  },
+];

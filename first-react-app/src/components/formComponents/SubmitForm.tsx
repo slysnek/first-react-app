f3ee0c32bf90abe2c0b63eef2a6ac38d3ae31630
Kirill <bloodwingest@gmail.com>
2023-04-04 01:02:00 +0300

    refactor: clean submit form

--- first-react-app/src/components/formComponents/SubmitForm.tsx
+++ first-react-app/src/components/formComponents/SubmitForm.tsx
@@ -1,52 +1,18 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import CheckboxInput from "./CheckboxInput";
-import DateInput from "./DateInput";
-import RadioInput from "./RadioInput";
-import SelectInput from "./SelectInput";
-import TextInput from "./TextInput";
-import UploadInput from "./UploadInput";
 
 export interface IFormData {
   (a: string[]): void;
 }
 
-type InputText = {
-  text: string;
-  hasError: boolean;
-};
-type InputDate = {
-  date: string;
-  hasError: boolean;
-};
-type InputSelect = {
-  rating: string;
-};
-type InputCheckbox = {
-  checked: boolean;
-};
-type InputRadio = {
-  whoMade: string;
-};
-type InputUpload = {
-  pic: string;
-};
-
 function SubmitForm(props: { isFormCorrect: (arg0: string[]) => void }) {
   const [popUpClass, setpopUpClass] = useState("error-hidden");
   const [popUpText, setpopUpText] = useState("You have errors in form. Please correct them.");
-  const [text, setText] = useState({ text: "Best text", hasError: false });
-  const [date, setDate] = useState({ date: "2023-01-01", hasError: false });
-  const [select, setSelect] = useState({ rating: "1" });
-  const [checkbox, setCheckbox] = useState({ checked: false });
-  const [radio, setRadio] = useState({ whoMade: "By me" });
-  const [upload, setUpload] = useState({ pic: "" });
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-    setValue,
   } = useForm({
     defaultValues: {
       songName: "Best Song In The World",
@@ -54,20 +20,20 @@ function SubmitForm(props: { isFormCorrect: (arg0: string[]) => void }) {
       select: "1",
       checkbox: false,
       radio: null,
-      upload: FileList,
+      upload: null,
     },
   });
 
-  function handleErrors(data: {
+  function handleData(data: {
     songName: string;
     date: string;
     select: string;
     checkbox: boolean;
     radio: null;
-    upload: FileList;
+    upload: null | FileList;
   }) {
-    const isTextWrong = text.hasError;
-    const isDateWrong = date.hasError;
+    const isTextWrong = errors.songName;
+    const isDateWrong = errors.date;
 
     if (isTextWrong || isDateWrong) {
       setpopUpClass("error-visible");
@@ -76,60 +42,31 @@ function SubmitForm(props: { isFormCorrect: (arg0: string[]) => void }) {
       setpopUpClass("confirm-visible");
       setpopUpText("You have successfully created a card.");
 
-      const checkBoxValuetoString = checkbox.checked;
+      let image = "";
 
-      const formData = [
-        text.text as string,
-        date.date as string,
-        select.rating as string,
-        checkBoxValuetoString.toString() as string,
-        radio.whoMade as string,
-        upload.pic as string,
-      ];
-      const formDataTest = [
-        data.songName,
-        data.date,
-        data.select,
-        data.checkbox.toString(),
-        data.radio as unknown as string,
-        URL.createObjectURL(data.upload[0]),
-      ];
-      console.log(formData);
-      props.isFormCorrect(formDataTest);
+      try {
+        image = URL.createObjectURL(data.upload![0]);
+      } catch (error) {
+        image = "";
+      } finally {
+        const formData = [
+          data.songName,
+          data.date,
+          data.select,
+          data.checkbox.toString(),
+          data.radio as unknown as string,
+          image,
+        ];
+        props.isFormCorrect(formData);
+      }
     }
   }
-
-  function handleText(data: InputText) {
-    setText(data);
-  }
-
-  function handleDate(data: InputDate) {
-    setDate(data);
-  }
-
-  function handleSelect(data: InputSelect) {
-    setSelect(data);
-  }
-
-  function handleCheckbox(data: InputCheckbox) {
-    setCheckbox(data);
-  }
-
-  function handleRadio(data: InputRadio) {
-    setRadio(data);
-  }
-
-  const handleInputChanges = (event: { target: { value: string } }) => {
-    const value = event.target.value;
-    console.log(value);
-    setValue("select", value);
-  };
 
   return (
     <form
       onSubmit={handleSubmit((data) => {
         console.log(data);
-        handleErrors(data);
+        handleData(data);
       })}
       className="submit-wrapper"
     >
@@ -154,7 +91,6 @@ function SubmitForm(props: { isFormCorrect: (arg0: string[]) => void }) {
       <p className="error-visible">{errors.date?.message}</p>
       <select
         {...register("select", {
-          onBlur: handleInputChanges,
           required: true,
         })}
       >
@@ -193,14 +129,8 @@ function SubmitForm(props: { isFormCorrect: (arg0: string[]) => void }) {
       </label>
       <p className="error-visible">{errors.radio?.message}</p>
       <input type="file" accept=".png,.jpg,.gif" {...register("upload")} />
-      {/*       <TextInput onTextInput={handleText}></TextInput>
-      <DateInput onDateInput={handleDate}></DateInput>
-      <SelectInput onSelectInput={handleSelect}></SelectInput>
-      <CheckboxInput onCheckboxInput={handleCheckbox}></CheckboxInput>
-      <RadioInput onRadioInput={handleRadio}></RadioInput>
-      <UploadInput onUploadInput={handleUpload}></UploadInput> */}
       <p className={popUpClass}>{popUpText}</p>
-      <button type="submit" className="submit-button" /* onClick={handleErrors} */>
+      <button type="submit" className="submit-button">
         Submit
       </button>
     </form>

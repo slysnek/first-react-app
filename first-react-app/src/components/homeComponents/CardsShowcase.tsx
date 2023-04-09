dfcc49ce5c9f1472a37aed491f1d6608af176481
Kirill <bloodwingest@gmail.com>
2023-04-10 01:13:31 +0300

    feat: add loading component

--- first-react-app/src/components/homeComponents/CardsShowcase.tsx
+++ first-react-app/src/components/homeComponents/CardsShowcase.tsx
@@ -4,35 +4,45 @@ import Card from "./Card";
 
 function Cards(props: { searchValue: string }) {
   const [cards, setCards] = useState<JSX.Element[] | null>(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function getArtistInfo() {
       const artistData = await lastFM.getArtistInfo(props.searchValue);
-      if (artistData) return artistData;
+      return artistData;
     }
 
     async function addArtistDataToArray() {
       const artistInfo: JSX.Element[] = [];
+      if (props.searchValue) setLoading(true);
       const data = await getArtistInfo();
-      if (data === undefined) return undefined;
+      if (data === undefined || data!.length === 0) {
+        setCards(null);
+        setLoading(false);
+        return;
+      }
       let count = 1;
       for (const artists of data) {
-        console.log(artists);
         artistInfo.push(
           <Card key={count} songArtist={artists.name} songImage={artists.image[1].text}></Card>
         );
         count++;
       }
       setCards(artistInfo);
+      setLoading(false);
     }
 
     addArtistDataToArray();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [props.searchValue]);
 
   return (
-    <div className="cards-wrapper">
-      {cards ? cards : <h3>Nothing to display. Search artists to display them.</h3>}
-    </div>
+    <>
+      {loading ? <h1>LOADING</h1> : null}
+      <div className="cards-wrapper">
+        {cards ? cards : <h3>Nothing to display. Search artists to display them.</h3>}
+      </div>
+    </>
   );
 }
 
